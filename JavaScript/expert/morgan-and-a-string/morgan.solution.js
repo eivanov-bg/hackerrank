6c8b6d824f09723dba978918d7fd66fb55383859
Emil Ivanov <emo_ivanov@yahoo.com>
2021-03-20 20:19:18 +0200

    Exit early from the recusrion if the end of the strings is reached

--- JavaScript/expert/morgan-and-a-string/morgan.solution.js
+++ JavaScript/expert/morgan-and-a-string/morgan.solution.js
@@ -1,29 +1,29 @@
 'use strict';
 
 function morganAndString(a, b, currentResult = '', memo = {}) {
-    let res = '';
     const key = a + ', ' + b;
 
     if (key in memo) return memo[key];
 
-    if (a === '') {
-        res = b;
-    } else if (b === '') {
-        res = a;
-    } else if (a[0] < b[0]) {
-            res = a[0] + morganAndString(a.slice(1), b, currentResult, memo)
-        } else if (a[0] > b[0]) {
-            return b[0] + morganAndString(a, b.slice(1), currentResult, memo);
-        } else {
-            const left = a[0] + morganAndString(a.slice(1), b, currentResult, memo);
-            const right = b[0] + morganAndString(a, b.slice(1), currentResult, memo);
+    if (a === '') return b;
+    if (b === '') return a;
 
-            if (left < right) {
-                res = left;
-            } else {
-                res = right
-            }
+    let res = '';
+
+    if (a[0] < b[0]) {
+            res = a[0] + morganAndString(a.slice(1), b, currentResult, memo)
+    } else if (a[0] > b[0]) {
+        return b[0] + morganAndString(a, b.slice(1), currentResult, memo);
+    } else {
+        const left = a[0] + morganAndString(a.slice(1), b, currentResult, memo);
+        const right = b[0] + morganAndString(a, b.slice(1), currentResult, memo);
+
+        if (left < right) {
+            res = left;
+        } else {
+            res = right
         }
+    }
 
     memo[key] = currentResult + res;
     return memo[key];
