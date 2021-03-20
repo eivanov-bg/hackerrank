cd204160bfd4273b46a2a54aea36531e35dbf613
Emil Ivanov <emo_ivanov@yahoo.com>
2021-03-20 22:13:02 +0200

    More test cases for camel case challenge

--- JavaScript/easy/camel-case/camel-case.test.js
+++ JavaScript/easy/camel-case/camel-case.test.js
@@ -2,4 +2,9 @@ const camelCase = require('./camel-case.solution');
 
 test( 'verify correctness', () => {
     expect(camelCase('saveChangesInTheEditor')).toBe(5);
+    expect(camelCase('')).toBe(0);
+    expect(camelCase('abc')).toBe(1);
+    expect(camelCase('aBCD')).toBe(4);
+    expect(camelCase('a1B2C3')).toBe(3);
+    expect(camelCase('oneTwoThree')).toBe(3);
 })
